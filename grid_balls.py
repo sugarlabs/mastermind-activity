11484b188f6b9ad73682da748f5e463cf0075cce
Anurag Singh <principalquantum30@gmail.com>
2024-04-16 20:48:59 +0530

    Revert "Port to python3 (2to3)"
    This reverts commit d02687f6c9d999e4c0019503be90e6ea0a7ff091.

--- grid_balls.py
+++ grid_balls.py
@@ -49,7 +49,7 @@ class GridBalls(Gtk.Grid):
         self.show_all()
 
     def clear(self):
-        for x in list(self.balls.keys()):
+        for x in self.balls.keys():
             for ball in self.balls[x]:
                 if ball is not None:
                     self.remove(ball)
@@ -174,7 +174,7 @@ class GridBalls(Gtk.Grid):
         self.show_all()
 
     def clear_callbacks(self, unset=False):
-        for idx in list(self.callbacks_ids.keys()):
+        for idx in self.callbacks_ids.keys():
             ball = self.callbacks_ids[idx]
             ball.disconnect(idx)
 
